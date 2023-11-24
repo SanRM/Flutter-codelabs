fec2edf829f971aafcc0159e69d37e67b64ba64d
SanRM <santiagoromom95@gmail.com>
2023-11-24 00:02:18 -0500

    flutter patterns wip

---
+++ patterns_codelab/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'data.dart';
+
+void main() {
+  runApp(const DocumentApp());
+}
+
+class DocumentApp extends StatelessWidget {
+  const DocumentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: DocumentScreen(
+        document: Document(),
+      ),
+    );
+  }
+}
+
+class DocumentScreen extends StatelessWidget {
+  final Document document;
+
+  const DocumentScreen({
+    required this.document,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    //var metadataRecord = document.getMetadata();
+    var (title, :modified) = document.getMetadata(); // New
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text('Last modified $modified'),
+          ),
+        ],
+      ),
+    );
+  }
+}
